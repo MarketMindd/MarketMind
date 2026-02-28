cb6d2ef264460312b8b86c43e32f0134d9221575
aylongo <aylongo10@gmail.com>
2026-03-01 00:13:06 +0200

    feat(): init router

---
+++ apps/frontend/src/app/views/signInn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      {/* TODO: add form fields and logic */}
+    </div>
+  );
+};
---
+++ apps/frontend/src/app/views/signUpp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SignUp: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* TODO: add form fields and logic */}
+    </div>
+  );
+};
