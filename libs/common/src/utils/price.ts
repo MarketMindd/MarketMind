48f056c9ecc0bd41a63765fb7664c4ae6826f51b
ponda <bashvitzben@gmail.com>
2026-04-29 17:04:46 +0300

    fix(): pull request

--- libs/common/src/utils/price.ts
+++ libs/common/src/utils/price.ts
@@ -1,7 +1,7 @@
 export const calculatePriceChange = (currentPrice: number, percentChange: number) => {
   const decimalChange = percentChange / 100;
   const originalPrice = currentPrice / (1 + decimalChange);
-  const dollarChange = currentPrice - originalPrice;
+  const priceChange = currentPrice - originalPrice;
 
-  return Number(dollarChange.toFixed(2));
+  return Number(priceChange.toFixed(2));
 };
